9107689b6939d366d5b734b51b7d5d0e0b4be403
unknown <19a91a0434@aec.edu.in>
2022-12-10 11:16:00 +0530

    Total amount paid

---
+++ productcount.py
@@ -0,0 +1,8 @@
+handbagcount=int(input("enter no of handbags"))
+shoescount=int(input("enter no of shoes"))
+batcount=int(input("enter no of bats"))
+handbagquan=50
+shoesquan=60
+batquan=70
+totalamount=handbagcount*handbagquan+shoescount*shoesquan+batcount*batquan
+print(totalamount)
